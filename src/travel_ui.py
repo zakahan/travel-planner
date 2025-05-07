31ca64bdaf19be9b121170dfcad6320613b1888e
hanzhi <jupiteroroz@outlook.com>
2025-05-07 16:15:04 +0800

    fix travel_ui.py

--- src/travel_ui.py
+++ src/travel_ui.py
@@ -9,12 +9,59 @@ logger = get_logger(__name__)
 st.set_page_config(page_title="ADK-Powered Travel Planner", page_icon="✈️", layout="wide")
 # st.title("🌍 ADK-Powered Travel Planner")
 
+def replan(suggest: str, part: str):
+    payload = {
+                "origin": origin,
+                "destination": destination,
+                "start_date": str(start_date),
+                "end_date": str(end_date),
+                "budget": budget,
+                "plan" : str(st.session_state.first_response),
+                "suggest": f"用户对{part}提出了意见，意见内容是{suggest}"
+            }
+    response = requests.post("http://localhost:8090/replan", json=payload)
+    if response.ok:
+        data = dict(response.json()['data'])
+        for key, value in data.items():
+            st.session_state.first_response[key] = value
+        # 强制页面重新运行
+        st.rerun()
+
+
+def booking(suggest: str):
+    print("aaaaaa")
+    payload = {
+        "plan": str(st.session_state.first_response),
+        "suggest": suggest
+    }
+    response = requests.post("http://localhost:8090/booking", json=payload)
+    print(f"response:{response.json()}")
+    if response.json()['code']==200:
+        st.info(f"预定成功：{response.json()['data']}")
+    else:
+        st.error("预定失败")
+
 
 # 初始化会话状态
 if 'first_response' not in st.session_state:
-    st.session_state.first_response = None
+    st.session_state.first_response = {}
 if 'user_input' not in st.session_state:
     st.session_state.user_input = None
+
+if 'show_input_flight' not in st.session_state:
+    st.session_state.show_input_flight = False
+
+if 'show_input_stay' not in st.session_state:
+    st.session_state.show_input_stay = False
+
+if 'show_input_activities' not in st.session_state:
+    st.session_state.show_input_activities = False
+
+if 'show_input_booking' not in st.session_state:
+    st.session_state.show_input_booking = False
+
+if 'booking_text' not in st.session_state:
+    st.session_state.booking_text = ""
 
 with st.sidebar:
 
@@ -38,12 +85,7 @@ with st.sidebar:
             }
             response = requests.post("http://localhost:8090/plan", json=payload)
             if response.ok:
-                st.session_state.first_response = response.json()
-                # st.subheader("✈️ Flights")
-                # st.markdown(st.session_state.first_response["flights_agent"])
-                # st.subheader("🏨 Stays")
-                # st.markdown(st.session_state.first_response["stay_agent"])
-                # st.subheader("🗺️ Activities")
+                st.session_state.first_response = response.json()['data']
                 # st.markdown(st.session_state.first_response["activities_agent"])
             else:
                 st.error("Failed to fetch travel plan. Please try again.")
@@ -54,31 +96,60 @@ with st.sidebar:
 
 col1, col2, col3 = st.columns(3)
 
-if st.session_state.first_response is not None:
-    resp = st.session_state.first_response
-    if resp['code'] == 200:
-        # 在第一栏中添加内容
-        with col1:
-            st.subheader("✈️ Flight")
-            if "flight_agent" in resp['data']:
-                st.markdown(resp['data']["flight_agent"])
-            else:
-                st.markdown("Failed to fetch this plan")
+if len(st.session_state.first_response) != 0:
+    # 在第一栏中添加内容
+    with col1:
+        st.subheader("✈️ Flight")
+        if "flight_agent" in st.session_state.first_response:
+            st.markdown(st.session_state.first_response["flight_agent"])
+        else:
+            st.markdown("Failed to fetch this plan")
+        if st.button("Another Batch 🏃‍♂️"):
+            st.session_state.show_input_flight = not st.session_state.show_input_flight
+        if st.session_state.show_input_flight:
+            user_input_flight = st.chat_input("Any suggestion about flight?")
+            if user_input_flight is not None:
+                replan(user_input_flight, "flight_agent")
+                st.session_state.show_input_flight = not st.session_state.show_input_flight
+    # 在第二栏中添加内容
+    with col2:
+        st.subheader("🏨 Stay")
+        if "stay_agent" in st.session_state.first_response:
+            st.markdown(st.session_state.first_response["stay_agent"])
+        else:
+            st.markdown("Failed to fetch this plan")
+        if st.button("Another Batch 🙋"):
+            st.session_state.show_input_stay = not st.session_state.show_input_stay
+        if st.session_state.show_input_stay:
+            user_input_stay = st.chat_input("Any suggestion about stay?")
+            if user_input_stay is not None:
+                replan(user_input_stay, "saty_agent")
+                st.session_state.show_input_stay = not st.session_state.show_input_stay
+    # 在第三栏中添加内容
+    with col3:
+        st.subheader("🗺️ Activities")
+        if "activities_agent" in st.session_state.first_response:
+            st.markdown(st.session_state.first_response["activities_agent"])
+        else:
+            st.markdown("Failed to fetch this plan")
 
-        # 在第二栏中添加内容
-        with col2:
-            st.subheader("🏨 Stay")
-            if "stay_agent" in resp['data']:
-                st.markdown(resp['data']["stay_agent"])
-            else:
-                st.markdown("Failed to fetch this plan")
+        if st.button("Another Batch 😼"):
+            st.session_state.show_input_activities = not st.session_state.show_input_activities
+        if st.session_state.show_input_activities:
+            user_input_activities = st.chat_input("Any suggestion about activities?")
+            if user_input_activities is not None:
+                replan(user_input_activities, "activities_agent")
+                st.session_state.show_input_activities = not st.session_state.show_input_activities
 
-        # 在第三栏中添加内容
-        with col3:
-            st.subheader("🗺️ Activities")
-            if "activities_agent" in resp['data']:
-                st.markdown(resp['data']["activities_agent"])
-            else:
-                st.markdown("Failed to fetch this plan")
-    else:
-        st.error("Failed to fetch travel plan. Please try again.")
+
+    # st.markdown(st.session_state.booking_text)
+    if st.button("Booking 🏨"):
+        st.session_state.show_input_booking = not st.session_state.show_input_booking
+
+    # 将 chat_input 移到状态判断代码块中
+    if st.session_state.show_input_booking:
+        user_input = st.chat_input("Do you want to booking?")
+        if user_input is not None:
+            booking(user_input)
+
+           
--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ cd src
 python3 tools/prepare/prepare_vector.py
 
 uvicorn apis:app --port 8090 &
-# uvicorn agents.flight_agent.__main__:app --port 8001 &
-# uvicorn agents.stay_agent.__main__:app --port 8002 &      
-# uvicorn agents.activities_agent.__main__:app --port 8003 &
-# uvicorn agents.booking_agent.__main__:app --port 8004 &
+
+
 streamlit run travel_ui.py
